3a11da46f5baaf55f946e7e2390b958218de2071
LambdaGaming <starwars18m@gmail.com>
2023-09-02 12:23:53 -0400

    Update to exiled 8

--- SCP198/Plugin.cs
+++ SCP198/Plugin.cs
@@ -8,14 +8,13 @@ namespace SCP198
 	public class Plugin : Plugin<Config>
 	{
 		private EventHandlers EventHandlers;
-		public override Version Version { get; } = new Version( 2, 1, 0 );
-		public override Version RequiredExiledVersion { get; } = new Version( 7, 0, 0 );
+		public override Version Version { get; } = new Version( 2, 1, 1 );
+		public override Version RequiredExiledVersion { get; } = new Version( 8, 0, 0 );
 		public override PluginPriority Priority { get; } = PluginPriority.Medium;
 
 		public override void OnEnabled()
 		{
 			base.OnEnabled();
-			EventHandlers = new EventHandlers( this );
 			EventHandlers = new EventHandlers( this );
 			events.Player.ItemAdded += EventHandlers.OnItemPickup;
 			events.Server.RoundEnded += EventHandlers.OnRoundEnd;
